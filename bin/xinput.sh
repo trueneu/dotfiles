a6d1f186013743862f2c332281fd80958ac53d60
Pavel Gurkov <true.neu@gmail.com>
2021-02-04 20:37:17 +0100

    moar

--- bin/xinput.sh
+++ bin/xinput.sh
@@ -3,3 +3,7 @@
 xinput | grep -P '(Ultimate Gadget Laboratories Ultimate Hacking Keyboard|Razer Razer DeathAdder Elite)' | fgrep pointer | grep -Po '(?<=id=)\d+' | while read p_id ; do
     xinput --set-prop $p_id "libinput Natural Scrolling Enabled" 1
 done
+
+setxkbmap -layout 'us,us,ru' -variant 'dvp, ,dvp'
+
+xset dpms 0 0 0
