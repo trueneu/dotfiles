9635a22886f2b39d22cf54f1f12b92b1b26cabc9
Pavel Gurkov <true.neu@gmail.com>
2021-06-08 12:04:16 +0200

    moar

--- bin/xinput.sh
+++ bin/xinput.sh
@@ -2,6 +2,7 @@
 
 xinput | grep -P '(Ultimate Gadget Laboratories Ultimate Hacking Keyboard|Razer Razer DeathAdder)' | fgrep pointer | grep -Po '(?<=id=)\d+' | while read p_id ; do
     xinput --set-prop $p_id "libinput Natural Scrolling Enabled" 1
+    xinput --set-float-prop $p_id 'Coordinate Transformation Matrix' 0.5 0 0 0 0.5 0 0 0 1
 done
 
 xinput | grep -P 'Wacom Intuos Pro M Finger touch' | fgrep pointer | grep -Po '(?<=id=)\d+' | while read p_id ; do
